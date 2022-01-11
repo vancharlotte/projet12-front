cd47ff82762341d8c1065bef70da185afd78f057
charlotte <vanhuyse.charlotte@gmail.com>
2022-01-11 12:50:30 +0100

    add call to microservice-location

--- src/app/component/location/location.component.ts
+++ src/app/component/location/location.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+
+import { Location } from 'src/app/model/location-model';
+
 
 @Component({
   selector: 'app-location',
@@ -6,10 +11,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./location.component.scss']
 })
 export class LocationComponent implements OnInit {
+  location! : any;
 
-  constructor() { }
+  constructor(public auth: AuthService, public http : HttpClient) {}
 
   ngOnInit(): void {
   }
 
+  
+  callApi() {
+    this.http.get(
+      encodeURI(`http://localhost:9004/location/get/d2d4b202-1fa1-4934-9ebb-b2d3202ffa48`)).subscribe((location) => (this.location = JSON.stringify(location, null, 2))
+      );
+  }
 }
