872a85c653298df97c4291a15ae703f170fca9af
charlotte <vanhuyse.charlotte@gmail.com>
2022-01-15 00:20:46 +0100

    add geocoder, controlnavigation and marker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,8 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 // Import the HTTP interceptor from the Auth0 Angular SDK
 import { AuthHttpInterceptor } from '@auth0/auth0-angular';
 import { MapBoxComponentComponent } from './map-box-component/map-box-component.component';
+//import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
+
 
 
 
@@ -36,31 +38,29 @@ import { MapBoxComponentComponent } from './map-box-component/map-box-component.
       domain: 'dev-kiddymap.eu.auth0.com',
       clientId: '4xx8AmoU9nEEbWWWOQJpli8IRxuzr0p4',
       audience: 'https://localhost:9004/',
-       
-
-
-  httpInterceptor: {
-    allowedList: [
-     'http://localhost:9004/*',
-    { uri:'https://localhost:9004/*',
+      httpInterceptor: {
+        allowedList: [
+          'http://localhost:9004/*',
+          { uri:'https://localhost:9004/*',
   
-     tokenOptions: {
-       // The attached token should target this audience
-       audience: 'https://localhost:9004/',
+            tokenOptions: {
+            // The attached token should target this audience
+            audience: 'https://localhost:9004/',
 
-       // The attached token should have these scopes
-       scope : 'access:data, read:current_user'
-    
-     }
-    }
-  ]
-
-
-
-  }
+            // The attached token should have these scopes
+            scope : 'access:data, read:current_user'
+            }
+          }
+        ]
+      }
     }),
     HttpClientModule,
-    ],
+
+   /* NgxMapboxGLModule.withConfig({
+      accessToken: 'pk.eyJ1IjoidmFuY2hhcmxvdHRlIiwiYSI6ImNreDVteDZlajBwbjkycG81bXpvbTM0b3EifQ.2DT673epmd2M_5exl2sqYw', // Optional, can also be set per map (accessToken input of mgl-map)
+      geocoderAccessToken: 'pk.eyJ1IjoidmFuY2hhcmxvdHRlIiwiYSI6ImNreDVteDZlajBwbjkycG81bXpvbTM0b3EifQ.2DT673epmd2M_5exl2sqYw' // Optional, specify if different from the map access token, can also be set per mgl-geocoder (accessToken input of mgl-geocoder)
+    }),*/
+  ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true 
     },
--- src/app/map-box-component/map-box-component.component.ts
+++ src/app/map-box-component/map-box-component.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 import * as mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+
+//import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
+
 
 @Component({
-  selector: 'app-map-box-component',
+  selector: 'app-mapbox',
   templateUrl: './map-box-component.component.html',
   styleUrls: ['./map-box-component.component.scss']
+
 })
 export class MapBoxComponentComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
@@ -21,6 +25,26 @@ export class MapBoxComponentComponent implements OnInit {
     center: [2.2923472384059096, 48.86277359529868], // starting position [lng, lat]
     zoom: 12 // starting zoom
     });
+
+    map.addControl(new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken      })
+    );
+    map.addControl(new mapboxgl.NavigationControl());
+
+    // create the popup
+const popup = new mapboxgl.Popup({ offset: 25 }).setText(
+  'TOUR EIFFEL.'
+  );
+    
+
+
+    const marker1 = new mapboxgl.Marker()
+    .setLngLat([2.294481
+      , 48.858370])
+    .setPopup(popup)
+    .addTo(map);
+    
+
   }
 
   
