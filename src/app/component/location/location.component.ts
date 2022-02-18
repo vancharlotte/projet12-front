c154d7f74806c6783515ad06a2a62c7759cc8f95
charlotte <vanhuyse.charlotte@gmail.com>
2022-02-18 01:58:59 +0100

    add method suscription to verify favorite

--- src/app/component/location/location.component.ts
+++ src/app/component/location/location.component.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import { lastValueFrom, map, mergeMap, Observable, switchMap } from 'rxjs';
 
 import { Location } from 'src/app/model/location-model';
+import { Profil } from 'src/app/model/profil-model';
 
 
 
@@ -13,50 +15,104 @@ import { Location } from 'src/app/model/location-model';
 })
 export class LocationComponent implements OnInit {
   location! : Location;
-  locationId!: string;
-  profilId!: string; 
+  locationId!: any;
+  profil!: any; 
   profilString!: string;
   profilAuth!: any;
-  profil!: any;
+  data!: any;
+  exist!: boolean;
+sub!:any;
+
 
   constructor(public auth: AuthService, public http : HttpClient) {   
   }
 
-  ngOnInit(): void {
+   ngOnInit() {
 
     const queryString = window. location. search;
     const urlParams = new URLSearchParams(queryString);
+    this.locationId = urlParams.get('id');
+    
+    this.findLocation();
+    
+    const sequence$ = this.auth.user$.pipe(
+    switchMap(user => 
+       this.http.get(
+        encodeURI(`http://localhost:9004/profil/get/auth/`+ user?.sub)).pipe(
+          switchMap(data => 
+            this.http.get(
+              encodeURI(`http://localhost:9004/profil/favorite/exist/`+ JSON.parse(JSON.stringify(data, null, 2)).id + "/" + this.locationId))))));
+
+    sequence$.subscribe((boo) => ( this.exist = JSON.parse(JSON.stringify(boo, null, 2)), console.log(this.exist)));
+  
+  
+  
+}
+
+
+getProfilId(): Observable<any> {
+      return this.finduseridtest(this.sub).pipe(map(sub  => this.findusersubtest()));
+}
+
+
+
+findLocationtest(){
+  const queryString = window. location. search;
+    const urlParams = new URLSearchParams(queryString);
+    this.locationId = urlParams.get('id');
+
+  return this.http.get(
+    encodeURI(`http://localhost:9004/location/get/`+ this.locationId));
+}
+
+findusersubtest() : any{
+   this.auth.user$;
+
+  
+} 
+
+finduseridtest(subid:any){
+  return this.http.get(
+    encodeURI(`http://localhost:9004/profil/get/auth/`+ subid))
+}
+
+findLocation():Location{
+  const queryString = window. location. search;
+    const urlParams = new URLSearchParams(queryString);
     const locationId = urlParams.get('id');
 
-      this.http.get(
+    this.http.get(
         encodeURI(`http://localhost:9004/location/get/`+ locationId)).subscribe((locationJSON) => ( this.location = JSON.parse(JSON.stringify(locationJSON, null, 2)),
-           console.log(this.location.id)));
- 
+           console.log(this.location.name)));
 
-
-    this.auth.user$.subscribe(
-      (profilJSON) => (this.profilString = JSON.stringify(profilJSON, null, 2),
-      this.profilAuth = JSON.parse(this.profilString),
-  //   console.log("JSON object -", this.profilAuth.sub),
-
-     
-     this.http.get(
-       encodeURI(`http://localhost:9004/profil/get/auth/`+ this.profilAuth.sub)).subscribe((profilJSON2) => ( this.profil = JSON.parse(JSON.stringify(profilJSON2, null, 2)),
-          console.log("find profil id"),
-          this.http.get(
-            encodeURI(`http://localhost:9004/profil/favorite/exist/`+ this.profil.id + "/" + this.location.id)).subscribe((locationJSON) => ( this.location = JSON.parse(JSON.stringify(locationJSON, null, 2)),
-               console.log("look for favorite"))))
-     
-          
-          
-          
-          )));
-
-
-     ;
-
-    
+           return this.location;
   
+
+}
+
+getSubUser() : any{
+  this.auth.user$.subscribe((data:any) => {
+    if(data != null) {  
+      console.log(data.sub); 
+      return data.sub;
+    }
+    else {
+      console.log(data.sub);
+      return "prout";
+
+}
+  })
+}
+
+isFavorite(userid:string, locid:string) : boolean {
+
+
+  
+    this.http.get(
+          encodeURI(`http://localhost:9004/profil/favorite/exist/`+ userid + "/" +locid)).subscribe((locationJSON) => ( this.exist = JSON.parse(JSON.stringify(locationJSON, null, 2)),
+             console.log("look for favorite")));
+
+          return this.exist;
 }
   
   addToFavorites(){
@@ -67,4 +123,8 @@ export class LocationComponent implements OnInit {
     console.log("remove from favorites");
   }
   
+}
+
+function profilJSON(profilJSON: any) {
+  throw new Error('Function not implemented.');
 }
